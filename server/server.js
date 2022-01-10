3862e2039404bffdb0771e0a5e795bb858ead4ad
Black Nguyen <blackng10.5.1997@gmail.com>
2022-01-10 20:39:35 +0700

    update

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+const morgan = require("morgan");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
