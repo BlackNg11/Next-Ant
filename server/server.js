b846c03b8da1287a70e8a66ee28eab91f1337bbe
Black Nguyen <blackng10.5.1997@gmail.com>
2022-01-11 11:06:38 +0700

    update

--- server/server.js
+++ server/server.js
@@ -1,18 +1,34 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs";
+import mongoose from "mongoose";
+
 const morgan = require("morgan");
 require("dotenv").config();
 
+//db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Db connect"))
+  .catch((err) => console.log("DB Cconnect err", err));
+
+//creat express app
 const app = express();
 
+//midd
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+//route
+fs.readdirSync("./routes").map((r) =>
+  app.use("/api", require(`./routes/${r}`))
+);
 
+//port
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
